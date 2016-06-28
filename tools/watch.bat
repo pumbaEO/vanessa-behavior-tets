346cff394e7725538f28b3d7bebfa6aad249ac85
Shenja Sosna <shenja@sosna.zp.ua>
2016-06-29 00:29:09 +0300

    Исправил watcher для vscode

---
+++ tools/watch.bat
@@ -0,0 +1,15 @@
+echo off
+SET mypath=%~dp0
+SETLOCAL
+
+set BUILDPATH=.\build
+
+set RUNNER_IBNAME=/F"%BUILDPATH%/ib"
+rem set RUNNER_DBUSER=base
+rem set RUNNER_DBPWD=234567890
+
+SET RUNNER_ENV=production
+
+echo "watch"
+oscript %mypath%/runner.os watch compile.json
+exit /B
